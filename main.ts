fecea95f99e937c10f3067019577c50dcfd7baa5
fsolizn <fsolizv@gmail.com>
2020-08-27 15:40:42 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . f . . . 
+    . . . . . . . . . . . . f f f . 
+    f f f f f f f f f f f f f f 5 f 
+    . . . . f f f f f f f f f f f f 
+    . . . . f f f f f f f f f f . .
+    . . . . f . f . . . . f . f . . 
+    . . . . f . f . . . . f . f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
